3adc85b0f49b76bdb4c5bbed8f46f31c66e75827
Quentin Royo <150453973+qroyo@users.noreply.github.com>
2025-11-28 18:08:33 +0100

    Simplify script by removing Helm installation steps
    Removed installation of Helm and updating hosts file.

--- bonus-try/script.sh
+++ bonus-try/script.sh
@@ -2,12 +2,6 @@
 
 sudo kubectl create namespace gitlab
 
-sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-HOST_ENTRY="127.0.0.1 gitlab.qroyo.com"
-HOSTS_FILE="/etc/hosts"
-
-echo "$HOST_ENTRY" | sudo tee -a "$HOSTS_FILE"
-
 sudo helm repo add gitlab https://charts.gitlab.io/
 sudo helm repo update 
 sudo helm upgrade --install gitlab gitlab/gitlab \
