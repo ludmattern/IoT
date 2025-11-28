30947c316df167f1b0448b200031e457b3985107
lmattern <github.rfgrg@passmail.net>
2025-11-28 21:28:56 +0100

    Update playground deployment image to v2 and add ArgoCD application configuration

--- bonus-try/access_gitlab.sh
+++ bonus-try/access_gitlab.sh
@@ -6,4 +6,4 @@ echo -n "PASSWORD : "
   sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode
 echo
 
-sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 8080:8181 > /dev/null 2>&1 &
+sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 8080:8181
