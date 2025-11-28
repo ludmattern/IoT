fe132d7aaf4a0b176c6603df070309892be10d3a
Quentin Royo <150453973+qroyo@users.noreply.github.com>
2025-11-28 17:54:56 +0100

    Update GitLab setup script for new domain

--- bonus-try/script.sh
+++ bonus-try/script.sh
@@ -3,7 +3,7 @@
 sudo kubectl create namespace gitlab
 
 sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-HOST_ENTRY="127.0.0.1 gitlab.lmattern.com"
+HOST_ENTRY="127.0.0.1 gitlab.qroyo.com"
 HOSTS_FILE="/etc/hosts"
 
 echo "$HOST_ENTRY" | sudo tee -a "$HOSTS_FILE"
@@ -13,17 +13,14 @@ sudo helm repo update
 sudo helm upgrade --install gitlab gitlab/gitlab \
   -n gitlab \
   -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
-  --set global.hosts.domain=lmattern.com \
+  --set global.hosts.domain=qroyo.com \
   --set global.hosts.externalIP=0.0.0.0 \
   --set global.hosts.https=false \
   --timeout 600s
 
-#waitpodloc
-sudo kubectl wait --for=condition=ready --timeout=1200s pod -l app=webservice -n gitlab
-
 echo -n "USER: root GITLAB PASSWORD : "
-  sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode"
+  sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode
 
-sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 2>&1 >/dev/null &
+sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 &
 
 echo -n "link to gitlab : http://127.0.0.1"
